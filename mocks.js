f8a7318fbaeb57ee0c535a01bed60c3a9101947e
Travis.Horton <travis.horton@bodybuilding.com>
2021-04-29 10:24:26 -0600

    Add mocks

---
+++ mocks.js
@@ -0,0 +1,75 @@
+const mockAchievements = [
+  {
+    id: 1,
+    name: "BodySpace Wings",
+    description: "In Bodyspace, the user has set a goal, has selected and started a program, and is following 5 or more people.",
+    gradient_color_one: "#7EA2FD",
+    gradient_color_two: "#7ECEFD",
+    earned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/WingsAchieved.png",
+    unearned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/WingsUnachieved.png",
+    url: "https://achievements-api.cloud.bodybuilding.com/achievements/1.json"
+  },
+  {
+    id: 2,
+    name: "Selfie Badge",
+    description: "User has tracked a workout and taken a 'swelfie' along with it 20 times.",
+    gradient_color_one: "#960326",
+    gradient_color_two: "#D11A42",
+    earned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/SelfieAchieved.png",
+    unearned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/SelfieUnachieved.png",
+    url: "https://achievements-api.cloud.bodybuilding.com/achievements/2.json"
+  },
+  {
+    id: 3,
+    name: "2000 Club",
+    description: "User has lifted 2000 lbs or more in one week, 4 times.",
+    gradient_color_one: "#000000",
+    gradient_color_two: "#0D3461",
+    earned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/2000Achieved.png",
+    unearned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/2000Unachieved.png",
+    url: "https://achievements-api.cloud.bodybuilding.com/achievements/3.json"
+  },
+  {
+    id: 4,
+    name: "Goodwill Community Badge",
+    description: "User has made 30 comments on posts.",
+    gradient_color_one: "#0C4CEF",
+    gradient_color_two: "#5137A9",
+    earned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/GoodwillAchieved.png",
+    unearned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/GoodwillUnachieved.png",
+    url: "https://achievements-api.cloud.bodybuilding.com/achievements/4.json"
+  },
+  {
+    id: 5,
+    name: "Boss Streak Badge",
+    description: "User is in a program and tracks all workouts for that program in a week for 3 consecutive weeks.",
+    gradient_color_one: "#133E5C",
+    gradient_color_two: "#227373",
+    earned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/StreakAchieved.png",
+    unearned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/StreakUnachieved.png",
+    url: "https://achievements-api.cloud.bodybuilding.com/achievements/5.json"
+  },
+  {
+    id: 6,
+    name: "25 Club",
+    description: "User tracks 25 workouts.",
+    gradient_color_one: "#E47B4B",
+    gradient_color_two: "#FAB46A",
+    earned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/25Achieved.png",
+    unearned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/25Unachieved.png",
+    url: "https://achievements-api.cloud.bodybuilding.com/achievements/6.json"
+  },
+  {
+    id: 7,
+    name: "Profile Badge",
+    description: "Upload a profile picture to your account.",
+    gradient_color_one: "#5C7A55",
+    gradient_color_two: "#C2C76C",
+    earned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/ProfileAchieved.png",
+    unearned_image_url: "https://bbcom-prod-content-cache.s3-us-west-2.amazonaws.com/www.bodybuilding.com/assets/images/achievements/ProfileUnachieved.png",
+    url: "https://achievements-api.cloud.bodybuilding.com/achievements/7.json"
+  }
+];
+
+// eslint-disable-next-line
+export { mockAchievements };
